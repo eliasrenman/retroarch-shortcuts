74890cf5237bc24911582859e2357afbaa117f73
Elias Renman <elias.renman00@gmail.com>
2024-04-22 23:05:40 +0200

    feat: shortcut generation

--- src/io.rs
+++ src/io.rs
@@ -1,6 +1,7 @@
 use crate::globals::PATH;
 use crate::logic::Rom;
 use crc32fast::Hasher;
+use mslnk::ShellLink;
 use serde_json::{json, to_string_pretty, Value};
 use std::fs::{metadata, read_dir, read_to_string, write, File};
 use std::io::Read;
@@ -9,6 +10,7 @@ use std::path::{Path, PathBuf};
 pub fn path_exists(path: &str) -> bool {
     metadata(path).is_ok()
 }
+
 pub fn write_json_to_file(path: &str, json: Value) -> Result<(), std::io::Error> {
     write(path, to_string_pretty(&json).unwrap())
 }
@@ -71,15 +73,35 @@ pub fn read_crc_checksum(path: PathBuf) -> u32 {
     hasher.finalize()
 }
 
-pub fn write_rom_shortcut(output_dir: &str, retroarch_path: &str, rom: Rom) {
-    //TODO: add os specific instructions
-    let _ = write_json_to_file(
-        &format!("{}/{}.json", output_dir, rom.name),
-        json!({
-            "rom": rom.path,
-            "retroarch": retroarch_path,
-            "name": rom.name,
-            "core": rom.console.core_name()
-        }),
+pub fn write_rom_shortcut(output_dir: &str, retroarch_path: &str, core_dir: &str, rom: Rom) {
+    match std::env::consts::OS {
+        "linux" => {
+            let _ = write_json_to_file(
+                &format!("{}/{}.json", output_dir, rom.name),
+                json!({
+                    "rom": rom.path,
+                    "retroarch": retroarch_path,
+                    "name": rom.name,
+                    "core": format!("{}/{}", core_dir, rom.console.core_name())
+                }),
+            );
+        }
+        "windows" => create_windows_lnk(output_dir, retroarch_path, core_dir, rom),
+        // "macos" => println!("Running on macOS!"),
+        _ => println!("Running on an unknown operating system!"),
+    };
+}
+
+fn create_windows_lnk(output_dir: &str, retroarch_path: &str, core_dir: &str, rom: Rom) {
+    let target = format!(
+        r"{} -L {}/{} {}",
+        retroarch_path,
+        core_dir,
+        rom.console.core_name(),
+        rom.path
     );
+    let lnk = format!("{}/{}.lnk", output_dir, rom.name);
+    let sl = ShellLink::new(target).unwrap();
+    sl.set_name(Option(rom.name.to_string()));
+    sl.create_lnk(lnk).unwrap();
 }
--- src/logic.rs
+++ src/logic.rs
@@ -6,6 +6,7 @@ use crate::{
 use serde::Serialize;
 use serde_json::Value;
 use std::{
+    fs,
     path::{Path, PathBuf},
     process::exit,
 };
@@ -21,11 +22,13 @@ pub fn install() {
     println!("Managed to link {} roms", roms.len());
     let output_dir = config.get("outputDir").unwrap();
     let retro_arch_exec = config.get("retroArchExec").unwrap();
+    let core_dir = config.get("emulatorDir").unwrap();
     for rom in roms {
         println!("Writing {} to file", rom.name);
         let _ = write_rom_shortcut(
             output_dir.as_str().unwrap(),
             retro_arch_exec.as_str().unwrap(),
+            core_dir.as_str().unwrap(),
             rom,
         );
     }
@@ -42,7 +45,10 @@ fn map_roms(rom_paths: Vec<PathBuf>) -> Vec<Rom> {
             let checksum = read_crc_checksum(path.clone());
             let game_entry_option = database.get_by_crc(checksum);
             if game_entry_option.is_none() {
-                println!("Game entry not found");
+                println!(
+                    "Game entry not found for {}",
+                    path.file_name().unwrap().to_str().unwrap()
+                );
                 return None;
             }
             let game_entry = game_entry_option.unwrap();
@@ -77,4 +83,11 @@ fn get_rom_paths(config: Value) -> Vec<PathBuf> {
     }
 }
 
-pub fn uninstall() {}
+pub fn uninstall() {
+    println!("Uninstalling shortcuts");
+    let config = get_config();
+    let output_dir = config.get("outputDir").unwrap().as_str().unwrap();
+    let _ = fs::remove_dir_all(Path::new(output_dir));
+
+    exit(0);
+}
